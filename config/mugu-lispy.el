633b26ccbc294dc74179e501977f8277f7d8cd2a
Mugu-Mugu <amardavid3241@gmail.com>
2016-11-08 23:01:48 +0100

    lispy diminished by default

--- config/mugu-lispy.el
+++ config/mugu-lispy.el
@@ -1,11 +1,13 @@
 (use-package lispy
   :ensure
+  :diminish lispy-mode 
   :init
   (add-hook 'emacs-lisp-mode-hook (lambda () (lispy-mode 1))))
 
 (use-package evil-lispy
   :ensure
   :after 'lispy
+  :diminish evil-lispy-mode lispy-other-mode
   :init
   (add-hook 'emacs-lisp-mode-hook #'evil-lispy-mode)
   (add-hook 'clojure-mode-hook #'evil-lispy-mode)
