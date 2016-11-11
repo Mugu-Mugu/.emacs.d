9b6fac826358c85dc91618cc8979b523d8c15b24
Mugu-Mugu <amardavid3241@gmail.com>
2016-11-12 00:03:02 +0100

    ace-window keys changed

--- config/mugu-motion.el
+++ config/mugu-motion.el
@@ -1,20 +1,18 @@
 (use-package avy
   :ensure
   :defer
-  :config (progn
-            (setq avy-case-fold-search t)
-            (setq avy-all-windows nil)
-            (setq avy-background t)
-            (setq avy-style 'at-full)
-            (setq avy-keys (number-sequence ?a ?z))         
-            (setq avy-timeout-seconds 0.3)
-            )
-  )
+  :config
+  (setq avy-case-fold-search t)
+  (setq avy-all-windows nil)
+  (setq avy-background t)
+  (setq avy-style 'at-full)
+  (setq avy-keys (number-sequence ?a ?z))         
+  (setq avy-timeout-seconds 0.3))
 
 (use-package ace-window
   :ensure
   :defer
-  :config (setq aw-keys '(?h ?j ?k ?l ?q ?s ?d ?f ?g))
+  :config (setq aw-keys '(?a ?z ?e ?r ?t))
   ;; todo bigger font
   )
 
