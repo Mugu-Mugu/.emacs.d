66d3e2a0c98d0a82efbf78faad5174908191211f
Mugu-Mugu <amardavid3241@gmail.com>
2016-11-14 07:04:08 +0100

    slight refactoring of git hydras
    more modular

--- config/mugu-git-menu.el
+++ config/mugu-git-menu.el
@@ -3,19 +3,8 @@
 (require 'mugu-menu)
 (require 'mugu-core)
 
-(defhydra mugu-magit-visual-hydra
-  (:color amaranth :body-pre (set-mark-command nil))
-  ("j" next-line "↓ line")
-  ("k" previous-line "↑ line")
-  ("s" avy-goto-line "↓ line")
-  ("J" avy-goto-line-below "↓ line")
-  ("K" avy-goto-line-above "↑ line")
-  ("v" set-mark-command "↑ line")
-  ("q" hydra-pop "quit menu" :color blue :column nil)
-  ("<RET>" hydra-pop "" :color blue))
-
 (defhydra mugu-magit-visibility-hydra (:color red :hint nil)
-  " :hint nilvisibility hydra"
+  " visibility hydra"
   ("<tab>" magit-section-toggle "toggle at point" :column "Visibility - Section")
   ("C-<tab>" magit-section-cycle "cycle section")
   ("S-<tab>" magit-section-cycle-global "min/max all")
@@ -32,29 +21,43 @@
   ("-" magit-diff-less-context "- diff context")
   ("0" magit-diff-default-context "default diff context"))
 
-(defhydra mugu-magit-main-hydra (:color pink :inherit (mugu-magit-visibility-hydra/heads))
-  "
-                                  -- MAGIT --
-"
-  ("b" magit-section-up "↖ section" :column "2-Navigation")
+(defhydra mugu-magit-visual-hydra
+  (:color amaranth :body-pre (set-mark-command nil) :inherit (mugu-magit-visibility-hydra/heads))
+  ("j" next-line "↓ line" :column "Navigation")
+  ("k" previous-line "↑ line")
+  ("s" avy-goto-line "↓ line" :column "Navigation Avy")
+  ("J" avy-goto-line-below "↓ line")
+  ("K" avy-goto-line-above "↑ line")
+  ("v" set-mark-command "Toggle Selection" :column nil)
+  ("q" hydra-pop "quit menu" :color blue :column nil)
+  ("<RET>" hydra-pop "" :color blue))
+
+(defhydra mugu-magit-navigation-hydra (:color red :hint nil)
+
+  ("b" magit-section-up "↖ section" :column "Navigation")
   ("j" magit-section-forward "↓ section")
   ("k" magit-section-backward "↑ section")
   ("l" magit-section-forward-sibling "↓ section")
   ("h" magit-section-backward-sibling "↑ section")
-  ("g" magit-refresh "refresh" :column "3-Global Actions")
   ("v" (progn
-         (hydra-push 'mugu-magit-main-hydra/body)
-         (mugu-magit-visual-hydra/body)) "visual" :color blue "visual mode")
+         (hydra-push hydra-curr-body-fn)
+         (mugu-magit-visual-hydra/body)) "visual" :color blue "visual mode"))
+
+(defhydra mugu-magit-main-hydra (:color pink :inherit (mugu-magit-visibility-hydra/heads
+                                                       mugu-magit-navigation-hydra/heads))
+  "
+                                  -- MAGIT --
+"
+  ("r" magit-refresh "refresh" :column "3-Global Actions")
   ("$" magit-process-buffer "goto magit status")
-  ("G" magit-refresh-all "refresh all")
+  ("R" magit-refresh-all "refresh all")
   ("s" magit-stage "stage" :column "1-Actions")
   ("u" magit-unstage "unstage" :column "1-Actions")
   ("d" magit-discard "discard" :column "1-Actions")
+  ("g" push-button "visit" :column "1-Actions")
   ("q" nil "quit menu" :color blue :column nil)
   ("<RET>" magit-dispatch-popup "magit dispatch" :color blue)
   ("Q" magit-mode-bury-buffer "quit magit" :color blue))
 
-(after 'mugu-menu (mugu-menu-register-mode-menu 'magit-status-mode 'mugu-magit-main-hydra/body))
-(add-hook 'magit-status-mode-hook 'mugu-magit-main-hydra/body)
 
 (provide 'mugu-git-menu)
--- config/mugu-git.el
+++ config/mugu-git.el
@@ -6,6 +6,10 @@
   :bind (:map magit-mode-map ("SPC" . mugu-menu-main-menu)))
 
 (use-package mugu-git-menu
-  :after magit)
+  :after magit
+  :config
+  (mugu-menu-register-mode-menu 'magit-status-mode 'mugu-magit-main-hydra/body)
+  (add-hook 'magit-status-mode-hook 'mugu-magit-main-hydra/body))
+
 
 (provide 'mugu-git)
