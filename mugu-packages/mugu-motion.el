668db4db89da46b4734bfc2d7e70dd357eeecb46
Mugu-Mugu <amardavid3241@gmail.com>
2017-02-19 20:43:50 +0100

    More avy commands

--- mugu-packages/mugu-motion.el
+++ mugu-packages/mugu-motion.el
@@ -1,24 +1,48 @@
+;;; mugu-motion --- Summary
+;; tbc
+;;; Commentary:
+(require 'mugu-core)
+(require 'evil)
+
+;;; Code:
 (use-package avy
   :ensure
   :defer
+  :bind
+  (:map evil-normal-state-map
+        ("s" . avy-goto-char-2)
+        ("S" . avy-goto-line)
+        ("t" . evil-mugu-motion-t)
+        ("T" . evil-mugu-motion-T)
+        ("f" . evil-mugu-motion-f)
+        ("F" . evil-mugu-motion-F))
   :config
+  (require 'mugu-motion-utils)
   (setq avy-case-fold-search t)
   (setq avy-all-windows nil)
   (setq avy-background t)
   (setq avy-style 'at-full)
-  (setq avy-keys (number-sequence ?a ?z))         
-  (setq avy-timeout-seconds 0.3))
+  (setq avy-keys (number-sequence ?a ?z))
+  (setq avy-timeout-seconds 0.3)
+  (evil-define-key 'motion evil-motion-state-map
+    "s" 'avy-goto-char-2
+    "S" 'avy-goto-line
+    "t" 'evil-mugu-motion-t
+    "T" 'evil-mugu-motion-T
+    "f" 'evil-mugu-motion-f
+    "F" 'evil-mugu-motion-F))
+
+(use-package mugu-motion-utils
+  :defer)
 
 (use-package ace-window
   :ensure
   :defer
-  :config (setq aw-keys '(?a ?z ?e ?r ?t))
-  ;; todo bigger font
-  )
+  :config (setq aw-keys '(?a ?z ?e ?r ?t)))
 
 (use-package ace-link
   :ensure
   :defer)
 
-
 (provide 'mugu-motion)
+;;; mugu-motion ends here
