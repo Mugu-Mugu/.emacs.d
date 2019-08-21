e3693d3d1e0f73a6fb51006289cc208df0997839
David <david.amar@wakeo.co>
2019-08-21 23:27:59 +0200

    Use new logic buffer switch for project and make it more robust

--- src/mugu-project.el
+++ src/mugu-project.el
@@ -8,6 +8,7 @@
 (require 'mugu-counsel)
 (require 'mugu-directory)
 (require 'mugu-menu)
+(require 'mugu-buffer)
 (require 'projectile)
 (require 'dash)
 (require 'ht)
@@ -29,16 +30,12 @@ Note that this may fails when several project have the same name."
                                            (-remove-first 'identity (buffer-list))))))
     (ivy-read (format "Select a buffer in project %s" (projectile-project-name))
               buffers-in-project
-              :action 'switch-to-buffer)))
+              :action 'mugu-buffer-switch)))
 
 (defun mugu-project-root ()
   "Return the project of `current-buffer'.
-For non-file buffer, the behavious is special as they dont have a meaningful
-`default-directory'."
-  (let ((projectile-project-root-cache (ht-create)))
-    (if buffer-file-truename
-        (projectile-project-root)
-      (or projectile-project-root (mugu-project-root-from-name mugu-project-current-name)))))
+If buffer has no project, return current active one instead."
+  (or (mugu-project-buffer-project) (mugu-project-root-from-name mugu-project-current-name)))
 
 (defun mugu-project-name ()
   "Return the name of the current project."
@@ -47,19 +44,16 @@ For non-file buffer, the behavious is special as they dont have a meaningful
 (defun mugu-project-cd ()
   "Change directory to a child dir of the project."
   (interactive)
-  (mugu-counsel-fzf-dir (projectile-project-root))
+  (mugu-counsel-fzf-dir (mugu-project-root))
   (mugu-space-main))
 
 (defun mugu-project-buffer-project (&optional buffer)
   "Retrieve the project root of BUFFER.
 If BUFFER is not given, use `current-buffer' instead."
-  (let* ((buffer (or buffer (current-buffer)))
-         (buffer-cached-root (buffer-local-value 'projectile-project-root buffer))
-         (buffer-is-file (buffer-local-value 'buffer-file-truename buffer)))
-    (message "for buffer %s cached root is %s and is file is %s" buffer buffer-cached-root buffer-is-file)
-    (or buffer-cached-root
-        (and buffer-is-file (with-current-buffer buffer
-                              (setq-local projectile-project-root (mugu-project-root)))))))
+  (with-current-buffer (or buffer (current-buffer))
+    (if buffer-file-truename
+        (projectile-project-root)
+      projectile-project-root)))
 
 (defun mugu-project-set-buffer-project (project-name &optional buffer)
   "Define PROJECT-NAME for BUFFER.
@@ -70,26 +64,26 @@ If BUFFER is nil, it applies to `current-buffer' instead."
 (defun mugu-project-vc ()
   "Call VC backend for current project."
   (interactive)
-  (noflet ((projectile-project-root (&optional _dir) (mugu-project-root-from-name mugu-project-current-name)))
-    (projectile-vc)))
+  (projectile-vc (mugu-project-root)))
 
 (defun mugu-project-buffer-in-project-p (buffer &optional project-name)
   "Predicate determning if BUFFER is in project named PROJECT-NAME.
 If PROJECT-NAME is nil, current project name is used if any.
 Name is used instead of project because of ~ and other shnenanigans."
-  (equal (or project-name mugu-project-current-name)
-         (projectile-project-name (mugu-project-buffer-project buffer))))
+  (let ((buffer-project (mugu-project-buffer-project buffer)))
+    (equal (or project-name mugu-project-current-name)
+           (and buffer-project (projectile-project-name buffer-project)))))
 
 (defmenu mugu-project-menu
   (:color blue :hint nil :body-pre (unless (mugu-project-root)
-                                     (call-interactively 'projectile-switch-project)))
+                                     (call-interactively 'mugu-project-switch)))
   "
                               -- PROJECT MENU --
 
   -> Project Root : %s(mugu-project-root)
   -> Current Dir  : %s(mugu-directory-pwd)
 "
-  ("s" projectile-switch-project "switch project" :color red :column "1-Management")
+  ("s" mugu-project-switch "switch project" :color red :column "1-Management")
   ("a" projectile-add-known-project "register new project" :color red)
   ("u" projectile-remove-known-project "unregister project" :color red)
   ("v" mugu-project-vc "version control" :column "Other")
@@ -116,42 +110,55 @@ Name is used instead of project because of ~ and other shnenanigans."
   (mugu-project--set-project-of-buffer buffer)
   (switch-to-buffer buffer))
 
-(defun mugu-project-pop-to-buffer (buffer &rest args)
-  "Switch to BUFFER using `pop-to-buffer' switching project if required.
-ARGS are passed as is to `pop-to-buffer'"
-  (mugu-project--set-project-of-buffer buffer)
-  (apply #'pop-to-buffer buffer args))
-
-(defun mugu-project-save-wconf ()
+(defun mugu-project--save-wconf ()
   "Save current windows configuration."
   (when (mugu-project-name)
     (ht-set mugu-project-wconf-map (mugu-project-name) (current-window-configuration))))
 
-(defun mugu-project-restore-wconf (project-name)
+(defun mugu-project--restore-wconf (project-name)
   "Restore project windows configuration of project PROJECT-NAME.
 If there was no saved for PROJECT-NAME, clear all windows."
   (if (ht-contains? mugu-project-wconf-map project-name)
       (set-window-configuration (ht-get mugu-project-wconf-map project-name))
+    (select-window (frame-first-window))
     (delete-other-windows)))
 
 (defun mugu-project-after-switch ()
   "Actions after project switch: set current project and open a default buffer."
-  (setq mugu-project-current-name projectile-project-name)
-  (mugu-project-restore-wconf projectile-project-name)
   (unless (projectile-project-buffer-p (current-buffer) default-directory)
     (switch-to-buffer "*Messages*")))
 
+(defun mugu-project-switch (new-project)
+  "Switch to NEW-PROJECT changing wconf and projectile internal."
+  (interactive (list (ivy-read "Select a project: " (projectile-relevant-known-projects))))
+  (mugu-project--save-wconf)
+  (setq mugu-project-current-name (projectile-project-name new-project))
+  (projectile-switch-project-by-name new-project)
+  (mugu-project-after-switch)
+  (mugu-project--restore-wconf (projectile-project-name new-project)))
+
+(defun mugu-project--switch-maybe (new-buffer)
+  "Switch project in NEW-BUFFER if it is different than current buffer."
+  (let* ((current-project (mugu-project-root-from-name mugu-project-current-name))
+         (old-project (or (mugu-project-buffer-project (current-buffer)) current-project))
+         (new-project (mugu-project-buffer-project new-buffer))
+         (old-project-name (projectile-project-name old-project))
+         (new-project-name (if (projectile-project-p new-project)
+                               (projectile-project-name new-project)
+                             "-")))
+    (when (not (equal new-project-name old-project-name))
+      (mugu-project-switch new-project))))
+
 (defun mugu-project-activate ()
   "Configure perspective and projectile in a coherent feature."
-  (add-hook 'projectile-before-switch-project-hook 'mugu-project-save-wconf)
-  (add-hook 'projectile-find-dir-hook 'mugu-project-save-wconf)
-  (setq projectile-switch-project-action 'mugu-project-after-switch))
+  (add-hook 'mugu-buffer-before-switch-functions 'mugu-project--switch-maybe)
+  (setq projectile-switch-project-action 'ignore))
 
 (defun mugu-project-hyper-star ()
   "Search thing at point in project."
   (interactive)
   (let ((ivy-hooks-alist '((t . mugu-ivy-active-menu))))
-    (counsel-rg (thing-at-point 'symbol t) (projectile-project-root))))
+    (counsel-rg (thing-at-point 'symbol t) (mugu-project-root))))
 
 (provide 'mugu-project)
 ;;; mugu-project ends here
