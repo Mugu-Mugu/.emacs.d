00cb50b05d239bb59a0e176f1e7996e3de15b0cc
David <david.amar@wakeo.co>
2019-09-09 17:07:33 +0200

    Apply automatic prettier on save

--- conf/mugu-conf-javascript.el
+++ conf/mugu-conf-javascript.el
@@ -7,13 +7,27 @@
 (require 'mugu-menu)
 
 ;; * begin:
+(use-package js-mode
+  :straight nil
+  :defer
+  :custom
+  (js-jsx-indent-level 2)
+  (js-indent-level 2))
+
+(use-package prettier-js
+  :hook (js-mode . prettier-js-mode)
+  :delight
+  :custom
+  (prettier-js-show-errors 'echo))
+
 (use-package mugu-js
   :straight nil
   :hook
   (js-mode . lsp)
   (js-mode . mugu-js-configure-file)
   :config
-  (mugu-js-configure-package))
+  (mugu-js-configure-package)
+  )
 
 (use-package js2-mode
   :disabled "Because js-mode is better on 27 and is officialy recommanded by js2 team"
