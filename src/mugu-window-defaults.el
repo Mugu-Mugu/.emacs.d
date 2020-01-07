c1dff9b54f3ba371bd460034ac1660b4ea3f36ef
Mugu-Mugu <amardavid3241@gmail.com>
2020-01-07 20:42:41 +0100

    More side window configuration

--- src/mugu-window-defaults.el
+++ src/mugu-window-defaults.el
@@ -8,9 +8,11 @@
   (winner-mode 1)
   (mugu-window-configure-side-window "\\*Help\\*" 'right 80)
   (mugu-window-configure-side-window "\\*Warnings\\*" 'bottom 10)
+  (mugu-window-configure-side-window "\\*lispy-help\\*" 'right 80)
   (mugu-window-configure-side-window "\\*Apropos\\*" 'right 80)
   (mugu-window-configure-side-window "\\*Backtrace\\*" 'bottom 0.2)
   (mugu-window-configure-side-window "\\*Shell Command Output\\*" 'bottom 0.5)
+  (mugu-window-configure-side-window "\\*Messages\\*" 'top 0.3)
 
   (setq display-buffer-base-action
         '((display-buffer-reuse-window display-buffer-same-window)
