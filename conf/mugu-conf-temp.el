de22d2d20059d80ed5bfa1d5539a8bad1f516f47
David <david.amar@wakeo.co>
2020-05-03 17:36:02 +0200

    Add some experimental package configuration

--- conf/mugu-conf-temp.el
+++ conf/mugu-conf-temp.el
@@ -14,6 +14,21 @@
   (setq posframe-inhibit-double-buffering t)
   (ivy-posframe-mode))
 
+(use-package docker :defer)
+(use-package sqlformat :defer
+  :custom
+  (sqlformat-command 'sqlformat)
+  (setq sqlformat-args '("-s2"))
+  :hook
+  (sql-mode . sqlformat-buffer))
+
+(use-package sql :defer
+  :straight nil
+  :general
+  (:keymaps 'sql-mode-map
+            [remap mugu-lang-format-buffer] #'sqlformat-buffer)
+  :config
+  (mugu-lang-activate-for-mode 'sql))
 
 (provide 'mugu-conf-temp)
 ;;; mugu-conf-temp ends here
